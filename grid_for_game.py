ea451e546c2aa03793440f0ea45c25501f652ac4
Koenel <k20111151@kcl.ac.uk>
2024-07-04 14:25:27 +0200

    grid for game and test for finding a specific element/position

---
+++ grid_for_game.py
@@ -0,0 +1,11 @@
+# grid for game
+grid = [["." for variable in range(10)] for variable in range(10)] #this creates a grid that is 10x10, 10 rows (=list) with 10 elements each
+
+
+for row in grid:
+    print(*row) # print the grid, the * puts each row (=list) on an individual line
+
+
+# search for a specific element. First index is the nth-row, second is the index on the row, i.e. "column"
+first_element= grid[0][0]
+print(first_element)
